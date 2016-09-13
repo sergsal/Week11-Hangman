30f34a7797142b3e0b86788bb7716d3c299eef03
sergsal <sergio.saldarriaga88@gmail.com>
2016-09-13 11:29:57 -0400

    update to letter.js

--- letter.js
+++ letter.js
@@ -1,2 +1,24 @@
 var imported = require('./word.js');
-console.log(imported.Words("a","bat"))
+var run = new imported("c","bat");
+
+lettersGuessed = [];
+matchedGuess = [];
+wordGuessed = false;
+function Display (guess, gameWord) {
+	var wordView = "";
+	this.wordArray = gameWord.split("");
+	this.guess = guess;
+	this.lines = function(){
+		for(var i=0; i < this.wordArray.length; i++){
+			if (matchedGuess.indexOf(this.wordArray[i]) != -1){
+				wordView += this.wordArray[i];				
+			}else{
+				wordView += ' _ ';
+			};
+		};
+		console.log(wordView)
+	};
+};
+var check = new Display ("c","bat")
+run.updateGuess();
+check.lines();
--- word.js
+++ word.js
@@ -1,26 +1,26 @@
-exports.guesses = function Words (guess, gameWord) {
-	this.selWord = gameWord;
-	this.wordArray = this.selWord.split("");
+function Words (guess, gameWord) {
+	this.wordArray = gameWord.split("");
 	this.guess = guess;
-	this.lettersGuessed = [];
-	this.matchedGuess = [];
-	this.wordGuessed = false;
-	this.checkGuess = function () {
+	this.goodGuess = function () {
 		for (i=0; i < this.wordArray.length; i++) {
-			if ((guess === this.wordArray[i]) && (this.matchedGuess.indexOf(guess) == -1)) {
-				this.matchedGuess.push(guess);
+			if ((guess === this.wordArray[i]) && (matchedGuess.indexOf(guess) == -1)) {
+				matchedGuess.push(guess);
 				// this.lettersGuessed.push(guess);
 				
 				// console.log(this.lettersGuessed);
-			} else {
-				this.lettersGuessed.push(guess);
-				// console.log(this.lettersGuessed);
-
-			}; //end of if/else statment
+			}
 		}; //end of for loop
-		console.log(this.matchedGuess);
-		console.log(this.lettersGuessed);
+		console.log(matchedGuess);
 	}; //end of checkguess
+	this.updateGuess = function () {
+		if ((lettersGuessed.indexOf(guess) == -1) && (this.wordArray.indexOf(guess) == -1)){
+			
+			lettersGuessed.push(guess);
+			console.log(lettersGuessed);
+		};
+	}
 
 
 };
+
+module.exports = Words
